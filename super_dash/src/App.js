75739172401e08012585e66a44d7b64461d9d837
Nils Merkel <nmerkel@kth.se>
2021-03-29 15:00:07 +0200

    restores the App file

--- super_dash/src/App.js
+++ super_dash/src/App.js
@@ -2,24 +2,6 @@ function App() {
   return (
       <div className="App">
           <p>Hello World</p>
-          <button onClick={()=>{
-              const data = fetch("/news",{
-                  method:"GET"
-              }).then(response =>{
-                  if(response.status !== 200){
-                      throw new Error("response code not 200")
-                  }
-                  return response
-
-              }).then(response =>{
-                  return response.json()
-
-              }).catch(error => {
-                  console.log("There was some problem with the fetch", error)
-              })
-              console.log(data)
-          }}>
-              Test the server</button>
       </div>
   );
 }
