a553aa81c039c0d0f5b68d5175473b064123e1e8
Love Book <love.andre.book@gmai.com>
2021-03-31 17:39:19 +0200

    9-modified_todo_list/addedTestingFile

--- super_dash/src/views/todoView.js
+++ super_dash/src/views/todoView.js
@@ -1,41 +1,32 @@
-import React, { Component } from 'react'; // this will not be needed later
-// tasks and props will later belong to the model, this is just for testing
-let tasks = ['städa', 'Träna', 'Tvätta', 'Handla'];
-const props = {
-    tasks: tasks,
-    removeTask: function (task) { if(tasks.find(t => t === task)) { tasks = tasks.filter(t => t !== task); }},
-    addTask: function (task) {tasks = [task, ...tasks];}
-}
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col, Form} from "react-bootstrap";
 
-// This is the component we will keep
-// This implementation of state will change later since we get the props from the model. Its used here for testing only
-class TodoView extends Component {
+function TodoView(props) {
 
-    state = {
-        currentTasks: tasks
-    }
+    const addByKey = (event) => {
+        if(event.code === 'Enter') {
+         props.addTask(event.target.value);
+        }}
 
-    stateChange = () => {
-        this.setState({
-            currentTasks: tasks
-        })
-    }
+    // we only want to add the listener the first time the component is rendered
+    // this is solved with useEffect
+    React.useEffect(() => (
+        window.addEventListener('keydown', addByKey)
+    ))
 
-    render()
-    {
-        return (
-            <div>
-                <h3>TODO-list</h3>
-                <input type="text" id="myInput" placeholder="new task..."/>
-                <button onClick={x => {props.addTask(document.getElementById('myInput').value); this.stateChange();}}>Add</button>
-                <table>
-                    {[...tasks].map(t => {return <tr>
-                        <td>{t}</td>
-                        <td><button onClick={e => {props.removeTask(t); this.stateChange();}}>x</button></td>
-                    </tr> })
-                    }
-                </table>
+    return (
+        <div>
+            <h3>Todo</h3>
+            <Container>
+                {[...props.tasks].map(t => {
+                    return <Row><Form><Form.Check type={"checkbox"}/></Form><Col>{t}</Col>
+                           <Col><Button className={'btn btn-light'} onClick={e => {props.removeTask(t);}}>x</Button></Col></Row>})
+                }
+            </Container>
+            <input type="text" id="myInput" placeholder="new task..." onBlur={e => {props.addTask(e.target.value);}}/>
             </div>
-        )};
+        )
 }
 export default TodoView;
