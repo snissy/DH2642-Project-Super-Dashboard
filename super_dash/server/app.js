62ad6f4b9a9eb3a36726aef72e42a355c65a4cac
Nils Merkel <nmerkel@kth.se>
2021-04-05 12:34:28 +0200

    none-Doc-This adds documentation to the code for fetchin omni items.

--- super_dash/server/app.js
+++ super_dash/server/app.js
@@ -3,10 +3,15 @@ const app = express()
 const port = 5000
 const omniGetter = require('./omniGetter.js');
 
+// Set server to listen too localhost:5000/news
+
 app.get('/news',(req, res) =>{
 
+    // Server recieved http request, fetch omni items
     let newsItems = omniGetter.fetchNewsArticles();
     newsItems.then(finalNewItems=>{
+
+        // Sends json object with all newsItem
         res.json(finalNewItems)
         console.log("I've finalized news request, sending data\n#---------------------------#\n")
     })
--- super_dash/server/omniGetter.js
+++ super_dash/server/omniGetter.js
@@ -2,30 +2,44 @@ const axios  = require("axios").default;
 const cheerio = require('cheerio');
 
 function fetchNewsArticles(){
-    // TODO
-    // all nyhetsdata ligger i divar som har classen "teaser teaser--small "
-    // Structuren på detta är kankse inte helt hundra än.
 
+    // Fetching the html data from omni
     let omniRequest = axios.get('https://omni.se/senaste')
         .then(response => {
-            // ok response
+
             if (response.status !== 200) {
+                // Couldn't receive omni html
+
                 new Error("Could not get omni items")
             }
+            // ok response
             return response.data
         })
         .catch(error => {
+            // log error
             console.log(error)
         })
 
+    // return a promise were we parse and scrap article items
     return omniRequest.then(rawHtml => {
 
+        // Creating Json object to send.
         let newsItems = {articles: []};
+
+        //Using Cheerio to scrape data
         let $ = cheerio.load(rawHtml);
 
-        $(".teaser").each((index, element) => {
+        // Query cherrio that get the article items
+
+        $(".group--single").not(".group--sponsored").each((index, element) => {
+            // iterate of all newsItems
+
+            //get the div tag that contain all data
             let articleDiv = $(element)
+
             let articleImg = articleDiv.find("img")
+
+            // create article item.
             let article = {
                 title:articleDiv.find(".resource--title").text(),
                 text:articleDiv.find(".resource--text").text(),
@@ -35,7 +49,7 @@ function fetchNewsArticles(){
             }
             newsItems.articles.push(article)
         })
-
+        // sending newsitem
         return newsItems
 
     }).catch(error => {
