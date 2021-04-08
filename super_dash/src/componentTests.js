a74d63417aaf9fb968349f3fdf6c9784fdaf7dfc
Adam Särnell <Adam@Adams-MacBook-Air-7.local>
2021-04-08 18:58:55 +0200

    Added starwarsAPI

--- super_dash/src/componentTests.js
+++ super_dash/src/componentTests.js
@@ -1,9 +1,9 @@
 import React from "react";
 import TodoView from "./views/todoView";
 import BootstrapExampleView from "./views/bootstrap_example";
-import Clock from "./views/clockView";
 import ClockPresenter from "./presenters/clockPresenter";
 import SearchBarPresenter from "./presenters/searchBarPresenter";
+import SwapiSource from './api/starwarsAPI'
 
 // Use this component to test TodoView
 export function TopLevelTodo() {
@@ -24,12 +24,13 @@ export function TopLevelTodo() {
 
 
 
-// T
+// Bootstrap example code
 export function TopLevelBootstrapExample(){
     return (
         <BootstrapExampleView />
     )
 }
+
 //Clock component
  function TopLevelClock(){   
     const [time, setTime] = React.useState() 
@@ -44,5 +45,52 @@ export default TopLevelClock;
 export function TopLevelSearchBar(){
     return (
         <SearchBarPresenter test="hej123"/>
+    )
+}
+
+
+// ------- Testing code for the SWAPI API ---------
+export function TopLevelSWAPI(){
+    const [num, setNum] = React.useState(5);
+    // promise is initially set to null and setPromise is called to change the promise state.
+    const [promise, setPromise] = React.useState(null);
+
+    // On component creation: initial random search results with params = {}
+    React.useEffect( function(){ 
+        setPromise(SwapiSource.getSwapiDetails("people", 1)); 
+    }, [] ); 
+
+    // data and error are derived states from the promise state
+    const [data, setData] = React.useState(null);
+    const [error, setError] = React.useState(null);
+
+    React.useEffect( function(){ 
+        setData(null); 
+        setError(null); 
+        let cancelled=false;
+
+        // When search is performed, promise will update.
+        if(promise){   
+
+            promise.then( function(dt) {
+                if(!cancelled) setData(dt);
+
+            }).catch( function(er){ 
+                if(!cancelled) setError(er);
+            });
+        }
+        
+        return function(){ 
+            cancelled=true;
+        };
+    }, [promise] ); // stateMember
+
+    if(data)
+        console.log(data);
+
+    return(
+        <div>
+            It's working!
+        </div>
     )
 }
