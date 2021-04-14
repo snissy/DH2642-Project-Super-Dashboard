e26ed24238ebc8b54d3650672fb6b776c88a5cf4
Nils Merkel <nmerkel@kth.se>
2021-04-14 11:12:36 +0200

    #12-Feat This adds a reusable methods that can be used on array for various purpose

---
+++ super_dash/src/helpFunctions/arrayMethods.js
@@ -0,0 +1,11 @@
+export const sumFunc = array=> {
+    return array.reduce((a,b)=>{return a+b},0)
+};
+export const meanFunc = array=> {
+    return sumFunc(array)/array.length;
+}
+export const maxFunc = array=> {
+    return array.reduce((a,b)=>{return Math.max(a,b)},-Infinity);
+}
+// Code borrowed from https://www.30secondsofcode.org/js/s/most-frequent
+export const mostFrequentFunc = arr => Object.entries(arr.reduce((a, v) => {a[v] = a[v] ? a[v] + 1 : 1;return a;}, {})).reduce((a, v) => (v[1] >= a[1] ? v : a), [null, 0])[0];
