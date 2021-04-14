a5ada0d629d46ac1e877261684c6bb58b67cf5e9
Nils Merkel <nmerkel@kth.se>
2021-04-14 11:10:27 +0200

    #12-Feat This creates a reusable PromiseHook that can be used whenever a promise is used in the project.

---
+++ super_dash/src/presenters/promiseHook.js
@@ -0,0 +1,22 @@
+import React, {useEffect} from "react";
+
+function PromiseHook(promise){
+
+    const [data, setData] = React.useState(null);
+    const [error, setError] = React.useState(null);
+
+    React.useEffect(()=>{
+
+        setData(null);
+        setError(null);
+        if(promise){ // Promise must be truthy,  not null
+            promise
+                .then(data => setData(data))
+                .catch(er => setError(er))
+        }
+    },[promise])
+
+    return [data, error]
+
+}
+export default PromiseHook
