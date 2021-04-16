d6599f2cff54607238a511e85d37db52265251ea
Love Book <love.andre.book@gmai.com>
2021-04-16 09:44:24 +0200

    Moved useEffect from View to Presenter/Done with all tasks in issue - Love

--- super_dash/src/presenters/todoPresenter.js
+++ super_dash/src/presenters/todoPresenter.js
@@ -5,6 +5,19 @@ function TodoPresenter(props) {
 
     const [currentTasks, setTasks] = React.useState(props.model.tasks);
 
+    const addByKey = (event) => {
+        if(event.code === 'Enter') {
+            let t = event.target.value;
+            props.model.addTask(t);
+            setTasks(props.model.tasks);
+        }}
+
+    // we only want to add the listener the first time the component is rendered
+    // this is solved with useEffect
+    React.useEffect(() => (
+        window.addEventListener('keydown', addByKey)
+    ))
+
     return (
         <TodoView tasks={currentTasks}
                   addTask={x => {props.model.addTask(x); setTasks(props.model.tasks);} }
--- super_dash/src/views/todoView.js
+++ super_dash/src/views/todoView.js
@@ -5,18 +5,6 @@ import {Container, Row, Col, Form} from "react-bootstrap";
 
 function TodoView(props) {
 
-    const addByKey = (event) => {
-        if(event.code === 'Enter') {
-         let t = event.target.value;
-         props.addTask(t);
-        }}
-
-    // we only want to add the listener the first time the component is rendered
-    // this is solved with useEffect
-    React.useEffect(() => (
-        window.addEventListener('keydown', addByKey)
-    ))
-
     return (
         <div className={"container rounded p-3 my-3 bg-dark text-white"} style={{border: '1px solid'}}>
             <Container>
