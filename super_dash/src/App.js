195aa998d51539537bc6dabc2919dae7dd74da42
nmerkel <nmerkel@kth.se>
2021-04-19 17:27:03 +0200

    Update App.js

--- super_dash/src/App.js
+++ super_dash/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import background from "./img/Hoth.png"
-import {TopLevelSearchBar, TopLevelSWAPI, TopLevelClock} from "./componentTests"
+import {TopLevelClock} from "./componentTests"
 import './css/App.css'
 import TodoPresenter from "./presenters/todoPresenter";
-import WeatherPresenter from "./presenters/weatherPresenter";
+import SideBarPresenter from "./presenters/sidebarPresenter";
+import SearchBarPresenter from "./presenters/searchBarPresenter";
 
 function App(props) {
-  return (<div className={'weather'}><WeatherPresenter latitude={59.295273} longitude ={18.094105}/></div>);
+  return (
+
+      <div className={'App'} style={{backgroundImage: `url(${background})`}}>
+          <h2>Super Dash</h2>
+          <div><SideBarPresenter/></div>
+          <div id={'Clock'}><TopLevelClock/></div>
+          <div id={'Searchbar'}><SearchBarPresenter/></div>
+          <div id={'Todo-list'}><TodoPresenter model={props.model}/></div>
+      </div>
+  );
 }
 
 /* SWAPI Test code
