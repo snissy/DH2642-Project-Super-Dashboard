6a4bcbd51af0d296c419012d373a8dece3e7d70c
Nils Merkel <nmerkel@kth.se>
2021-04-30 23:04:38 +0200

    #55-Feat-Adds news view to the app just for development not deployment

--- super_dash/src/App.js
+++ super_dash/src/App.js
@@ -2,24 +2,29 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import background from "./img/Hoth.png"
 import {TopLevelClock} from "./componentTests"
-import './css/App.css'
-import './css/weatherView.css'
 import TodoPresenter from "./presenters/todoPresenter";
 import SideBarPresenter from "./presenters/sidebarPresenter";
 import SearchBarPresenter from "./presenters/searchBarPresenter";
 import WeatherPresenter from "./presenters/weatherPresenter";
+import NewsPresenter from "./presenters/newsPresenter";
+import './css/App.css'
+import './css/weatherView.css'
+import './css/newsView.css'
 
 function App(props) {
-  return (
 
-      <div className={'App'} style={{backgroundImage: `url(${background})`}}>
+/*
+  return (<div className={'App'} style={{backgroundImage: `url(${background})`}}>
           <h2>Super Dash</h2>
           <div><SideBarPresenter/></div>
           <div id={'Clock'}><TopLevelClock/></div>
           <div id={'Searchbar'}><SearchBarPresenter/></div>
           <div id={'Todo-list'}><TodoPresenter model={props.model}/></div>
-      </div>
-  );
+          <div id={'weatherboiii'}><WeatherPresenter longitude={18.072237} latitude={59.346677}/></div>
+      </div>);
+*/
+
+    return (<div><NewsPresenter/></div>)
 }
 
 /* SWAPI Test code
