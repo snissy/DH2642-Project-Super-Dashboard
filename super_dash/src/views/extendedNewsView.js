a11f35ade285b05774ea66fa52da9c9289a09d8f
Nils <nmerkel@kth.se>
2021-05-01 12:15:23 +0200

    #55-Refrac-This restructure the extended news views and adds icons

--- super_dash/src/views/extendedNewsView.js
+++ super_dash/src/views/extendedNewsView.js
@@ -1,23 +1,34 @@
+import backArrow from "../assets/svg/newsIcons/BackArrow";
+import omniArrow from "../assets/svg/newsIcons/OmniArrow";
+
 function ExtendedNewsView(props){
-    // TODO tillbaka knappen ska ha en symbol tycker jag.
-    // ()=>{location.href = props.newsItem.href}
 
     return (<div className={"extended-newsView " + props.isHidden} >
         <div className={"newsView-background"}>
             <div className={"extended-newsView-navigation"}>
-                <div className={"newsView-div-back newsButton"} onClick={e =>{props.goBack()}}>Back</div>
-                <div className={"newsView-div-omniButton newsButton"} onClick={e => {props.goToOmni(props.newsItem.href)}}>To omni</div>
-            </div>
-            <div className={"extended-newsView-card"}>
-                <div className={"extended-newsView-div-img-title"}>
-                    <img className={"newsView-picture-medium"}
-                         src={props.newsItem.imgSrc}
-                         alt={props.newsItem.imgTitle}/>
-                    <div className={"newsView-div-title"}>
-                        <p className={"newsView-title"}>{props.newsItem.title}</p>
+                <div className={"newsView-div-back newsButton"} onClick={e =>{props.goBack()}}>
+                    {backArrow}
+                    <div className={"newsButton-text"}>
+                        BACK
                     </div>
                 </div>
-                <div className={"newsView-div-text"}>
+                <div className={"newsView-div-omniButton newsButton"} onClick={e => {props.goToOmni(props.newsItem.href)}}>
+                    <div className={"newsButton-text"}>
+                        OMNI
+                    </div>
+                    {omniArrow}
+                </div>
+            </div>
+            <div className={"extended-newsView-card"}>
+                <div className={"newsView-div-title"}>
+                    <p className={"newsView-title"}>{props.newsItem.title}</p>
+                </div>
+                <div className={"extended-newsView-div-img-text"}>
+                    <div className={"extended-newsView-img-wrapper"}>
+                        <img className={"newsView-picture-medium"}
+                             src={props.newsItem.imgSrc}
+                             alt={props.newsItem.imgTitle}/>
+                    </div>
                     <p className={"newsView-text"}>{props.newsItem.text}</p>
                 </div>
             </div>
