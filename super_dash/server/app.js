258d0e57ad0d9631fb796a40f3acc2bc4bbb89cc
Nils <nmerkel@kth.se>
2021-05-03 09:35:36 +0200

    #59-Refrac-Restructuring the backend to server our react app.

--- super_dash/server/app.js
+++ super_dash/server/app.js
@@ -1,10 +1,40 @@
 const express = require("express")
 const app = express()
-const port = 5000
+const port = 3000
 const omniGetter = require('./omniGetter.js');
+const path = require("path");
+
+
+
+/*/
+To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
+
+The function signature is:
+
+express.static(root, [options])
+
+The root argument specifies the root directory from which to serve static assets. For more information on the options argument, see express.static.
+
+For example, use the following code to serve images, CSS files, and JavaScript files in a directory named public:
+
+app.use(express.static('public'))
+
+Now, you can load the files that are in the public directory:
+
+http://localhost:3000/images/kitten.jpg
+http://localhost:3000/css/style.css
+http://localhost:3000/js/app.js
+http://localhost:3000/images/bg.png
+http://localhost:3000/hello.html
+
+ */
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname,  '..',  'build',  'index.html'));
+});
 
 // Set server to listen too localhost:5000/news
-
 app.get('/news',(req, res) =>{
 
     // Server recieved http request, fetch omni items
@@ -15,6 +45,7 @@ app.get('/news',(req, res) =>{
         res.json(finalNewItems)
         console.log("I've finalized news request, sending data\n#---------------------------#\n")
     })
+    console.log()
     console.log("<--------------------------->\nI've received a news request")
 })
 
