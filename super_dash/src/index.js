d35df041f465f9570c0d90025a296242ccff2aee
hector gonzalez campos <hector-99hgc@hotmail.com>
2021-05-06 16:04:36 +0200

    Changes for saving the session LOCALY

--- super_dash/src/index.js
+++ super_dash/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import DashBoardModel from "./DashBoardModel";
+import {persistModel} from "./firebase/firebaseModel"
 
 
 const model = new DashBoardModel(['Destroy Deathstar', 'Kill Vader', 'Buy new Lightsaber', 'Save baby Yoda'])
-
+persistModel(model);
 ReactDOM.render(
   <React.StrictMode>
     <App model={model}/>
--- super_dash/src/presenters/logPresenter.js
+++ super_dash/src/presenters/logPresenter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {logOut, signInWithGoogle, user} from "../firebase/firebase";
+import {logOut, signInWithGoogle, user, auth, getuser} from "../firebase/firebase";
 import {Log} from "../views/logView";
 
 export function LogPresenter(props){
@@ -8,23 +8,24 @@ export function LogPresenter(props){
 
     //Boolean state of the user to know if is logged
     const [islogged,setLog] = React.useState(props.model.islogged);
-
+    const [pending, setPending] =  React.useState(true);
     //??? Observer added but not sure if it is necessary
     React.useEffect(function () {
+        auth().onAuthStateChanged((user) => {
+            console.log( user )
+            props.model.setUser(user)
+            setLog(props.model.islogged)
+            setPending(false)
+        });
         props.model.addObserver(()=> setLog(props.model.islogged))
+
     }, [])
 
     //function for login when the button is clicked
     const  handleLoginClick = () => {
 
-        //function from firebase-google which returns a promise
-        signInWithGoogle().then((res) => {
-            props.model.setUser(user())
-            setLog(props.model.islogged);
-            console.log(res)
-        }).catch((error) => {
-            console.log(error.message)
-        })
+        //function from firebase-google
+        signInWithGoogle();
 
     }
 
@@ -32,9 +33,8 @@ export function LogPresenter(props){
     const  handleLogoutClick = () => {
         //method from fireabse to logout
         logOut();
-        props.model.setUser(null);
-        setLog(props.model.islogged);
     }
+    if (pending) return <> Loading...</>
     return(
 
         <Log    islogged ={islogged}
