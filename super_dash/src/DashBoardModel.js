c8765d3931adcea6f71754c032b27a9ff2634d32
Nils <nmerkel@kth.se>
2021-05-06 19:52:39 +0200

    #72-Feat-This adds a coordinates object in the model for the weatherView

--- super_dash/src/DashBoardModel.js
+++ super_dash/src/DashBoardModel.js
@@ -36,6 +36,15 @@ class DashBoardModel {
         }
         this.user = null;
         this.islogged = false;
+
+        this.coords = {latitude: null, longitude: null};
+    }
+
+    setWeatherCoordinates(lat, long){
+
+        this.coords= {latitude: lat, longitude: long};
+
+        this.notifyObservers();
     }
   
     setUser (currentUser){
