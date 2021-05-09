66048f1443068d0a92472c37ce75a34c893d1bca
Love Book <love.andre.book@gmai.com>
2021-05-10 01:51:50 +0200

    functionality for (single) todolist basically finished

--- super_dash/src/views/todoView.js
+++ super_dash/src/views/todoView.js
@@ -7,11 +7,11 @@ function TodoView(props) {
 
     return (
         <div className={"container rounded p-2 bg-dark text-white"}>
-            <Container className={"ml-1 justify-content-between"}>
-                <input type="text" className={"form-control"} id="todoTitle" placeholder="new Todo list..."/>
+            <Container className={"ml-1 pr-0 justify-content-between"}>
+                <input type="text" className={"form-control"} id="todoTitle" placeholder="Title..." autoComplete={"off"}/>
                 {[...props.tasks].map(t => {
                     return <Row key={t} className={"justify-content-lg mb-2"}>
-                                <Col sm={10}><input className={"custom-checkbox"} type={"checkbox"}/><label for={"cBox"} id={"task"}>{'  ' + t}</label></Col>
+                                <Col sm={10}><input type={"checkbox"} onClick={e => {props.check(t);}}/><label id={"task"}>{' ' + t}</label></Col>
                                 <Col sm={1} pb={2}>
                                     <Button className={'btn btn-outline-dark close'} id={'removeButton'}
                                             onClick={e => {props.removeTask(t);}}><span aria-hidden="true">&times;</span></Button></Col>
@@ -20,11 +20,25 @@ function TodoView(props) {
                 <Row>
                     <Col sm={8}>
                         <input type="text" className={"form-control"} id="taskInput" placeholder="new task..." autoComplete={'off'}
-                               maxLength={17}
+                               maxLength={16}
                                onFocus={e => {e.target.value = '';}}
                                onBlur={e => {props.addTask(e.target.value);}}/>
                     </Col>
                 </Row>
+                <Row>
+                    <div className={"h-divider"}/>
+                </Row>
+                <Row>
+                    <Col sm={8}>
+                        <p>{props.checkedTasks.length + "   Finished tasks"}</p>
+                    </Col>
+                    <Col sm={3}><Button className={"btn btn-outline-secondary"} id={"removeButton"} onClick={x => {props.clear()}}>clear</Button></Col>
+                </Row>
+                {[...props.checkedTasks].map(t => {
+                    return <Row key={t} className={"justify-content-lg mb-2"}>
+                        <Col sm={10}><input type={"checkbox"} checked={true} onClick={e => {props.check(t);}}/><label for={"cBox"} id={"task"}>{'  ' + t}</label></Col>
+                    </Row>})
+                }
             </Container>
         </div>
         )
