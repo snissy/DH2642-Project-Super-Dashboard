c32aae7e3a3b47987b850d8331a373226315bcd8
Adam Särnell <sarnell@kth.se>
2021-05-09 15:59:58 +0200

    Made Clock follow MVP and now size is changed in App.css instead of in html style

--- super_dash/src/presenters/clockPresenter.js
+++ super_dash/src/presenters/clockPresenter.js
@@ -3,8 +3,18 @@ import React from "react";
 
 function ClockPresenter(props) {
     const [time, setTime] = React.useState(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
-    return <Clock time={time}
-                  updateTime ={setTime}/>
+
+    //This declared the state of time at the very beginning so it will create a interval for updating the clock each second 
+       
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })) 
+        }, 1000);
+        //if the clock component is finished it will be killed for stop using  memory
+        return () => clearInterval(interval);
+        }, []);
+
+    return <Clock time={time}/>
 }
 
 export default ClockPresenter;
--- super_dash/src/views/clockView.js
+++ super_dash/src/views/clockView.js
@@ -1,31 +1,8 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {Container} from 'react-bootstrap'
 
 function Clock (props) {
    
-
-        //This declared the state of time at the very beginning so it will create a interval for updating the clock 
-        //each second 
-       
-        React.useEffect(() => {
-            const interval = setInterval(() => {
-              props.updateTime(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })) 
-            }, 1000);
-            //if the clock component is finished it will be killed for stop using  memory
-            return () => clearInterval(interval);
-          }, []);
-
-     
-    
-        return (
-            <Container>
-                
-                  
-                    <h1 className="text-center m-1 display-1"> {props.time}</h1>
-               
-            </Container>
+  return (
+              <h1>{props.time}</h1>   
             )
-    
 }
 export default Clock;
