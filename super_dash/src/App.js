a6d92326c07e6ad3c37b8b226555149215939b9c
Nils <nmerkel@kth.se>
2021-05-10 11:36:20 +0200

    #81-Refrac-Updates the structure for the stored presenters

--- super_dash/src/App.js
+++ super_dash/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import background from "./img/Hoth.png"
+import background from "./assets/img/Hoth.png"
 import {TopLevelClock} from "./componentTests"
-import TodoPresenter from "./presenters/todoPresenter";
-import SideBarPresenter from "./presenters/sidebarPresenter";
-import SearchBarPresenter from "./presenters/searchBarPresenter";
-import WeatherPresenter from "./presenters/weatherPresenter";
-import CharacterDashPresenter from "./presenters/characterDashPresenter";
-import NewsPresenter from "./presenters/newsPresenter";
-import Visible from "./presenters/visibilityPresenter";
+import TodoPresenter from "./presenters/dashboardPresenters/todoPresenter";
+import SideBarPresenter from "./presenters/siderbarPresenters/sidebarPresenter";
+import SearchBarPresenter from "./presenters/dashboardPresenters/searchBarPresenter";
+import WeatherPresenter from "./presenters/dashboardPresenters/weatherPresenter";
+import CharacterDashPresenter from "./presenters/dashboardPresenters/characterDashPresenter";
+import NewsPresenter from "./presenters/dashboardPresenters/newsPresenter";
+import Visible from "./presenters/functionalPresenters/visibilityPresenter";
 import './css/App.css'
-import './css/weatherView.css'
 import './css/newsView.css'
+import './css/weatherView.css'
 import Draggable from 'react-draggable';
 
 function App(props) {
@@ -19,7 +19,7 @@ function App(props) {
     // Coordinates for all draggable components are use as state
     const [currentCoordinates, setCoordinates] = React.useState(props.model.coordinates);
 
-    // Tracks the position of a component, updates the model and state
+    // Tracks the position of a component, updates the models and state
     const trackPosition = (data, component) => {
         let newCoordinates = props.model.setCoordinates(component, data.deltaX, data.deltaY)
         setCoordinates(newCoordinates);
