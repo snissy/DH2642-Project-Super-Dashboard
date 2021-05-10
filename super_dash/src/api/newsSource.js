44eec14eb925b69287fffc0e282bb48146c3d955
Nils <nmerkel@kth.se>
2021-05-10 11:45:14 +0200

    #81-refrac rewrites the api call using the basicJsonApiCall

--- super_dash/src/api/newsSource.js
+++ super_dash/src/api/newsSource.js
@@ -1,26 +1,13 @@
+import BasicJsonApiCall from "./basicFetcher";
+
 const BASE_NEWS_URL = "/news"
 
 const  NewsSource={
-    // This function makes the actual api call to smhi
+
     apiCall(params) {
 
         // The actual fetch
-        return fetch(BASE_NEWS_URL, {
-            "method": "GET"
-        })
-            .then(response => {if(response.status !== 200){
-                // Something did not work
-                throw new Error("Response code was not 200")
-            }
-                // Everything went ok
-                return response;
-            })
-            // from HTTP headers to HTTP response data
-            .then(response => response.json()).catch(error => {
-
-                // The fetch failed
-                console.log("There was some problem with the fetch", error)
-            });
+        return BasicJsonApiCall({baseUrl:BASE_NEWS_URL});
     },
 
     getNewsItems(){
