0b2d188be7730aab99114e42088e2caf8254fd7e
hector gonzalez campos <hector-99hgc@hotmail.com>
2021-05-11 18:46:44 +0200

    More secure and fixed

--- super_dash/src/firebase/firebaseModel.js
+++ super_dash/src/firebase/firebaseModel.js
@@ -1,37 +1,40 @@
 import  {getUser} from "./firebase.js";
 import firebase from "firebase";
 export function persistModel(model) {
-    let uid= model.user.uid;
-    let loadingFromFirebase = false;  // boolean flag
-    model.addObserver(function () {
-        if (loadingFromFirebase) return
-        else {
-            console.log(model.user.uid)
-            console.log(uid);
-            console.log(model)
-            firebase.database().ref("users/" + uid + "/dashboardModel").set({
-                //planet: model.planet,
-                character: model.character,
-                tasks: model.tasks,
-                coordinates: model.coordinates
-            });
-        }
-    });
-
-    firebase.database().ref("users/" + uid  + "/dashboardModel").on("value", function (data) {
-        loadingFromFirebase = true;
-        try {
-            if (data.val()) {
-                console.log(data.val());
-                model.setCharacter(data.val().character, data.val().character.id);
-                model.setTasks(data.val().tasks || [])
-             //   model.setPlanet(data.val().planet )
+    if (model.islogged) {
+        let uid = model.user.uid;
+        let loadingFromFirebase = false;  // boolean flag
+        model.addObserver(function () {
+            if (loadingFromFirebase) return
+            if(!model.islogged) return
+            else {
+                firebase.database().ref("users/" + uid + "/dashboardModel").set({
+                    //planet: model.planet,
+                    character: model.character,
+                    tasks: model.tasks ,
+                    checkedTasks: model.checkedTasks,
+                    coordinates: model.coordinates,
+                    planet: model.planet
+                });
             }
-        } catch (e) {
-            console.log(e);
-        } finally {
-            loadingFromFirebase = false;
-        }
+        });
 
-    });
+        firebase.database().ref("users/" + uid + "/dashboardModel").on("value", function (data) {
+            loadingFromFirebase = true;
+            try {
+                if (data.val()) {
+                    console.log(data.val());
+                    model.setCharacter(data.val().character, data.val().character.id);
+                    model.setTasks(data.val().tasks || [], data.val().checkedTasks  || [])
+                    model.setPlanet(data.val().planet, data.val().planet.id)
+                    model.setAllCoordinates(data.val().coordinates)
+                }
+            } catch (e) {
+                console.log(e);
+            } finally {
+                loadingFromFirebase = false;
+            }
+
+        });
+    }
 }
