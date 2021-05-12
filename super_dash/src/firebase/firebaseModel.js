b8f640e9622fd32e0e0f0555f172029732fb86d2
hector gonzalez campos <hector-99hgc@hotmail.com>
2021-05-12 18:20:09 +0200

    Fixing everything

--- super_dash/src/firebase/firebaseModel.js
+++ super_dash/src/firebase/firebaseModel.js
@@ -12,8 +12,7 @@ export function persistModel(model) {
                 console.log("Firebase observer: Coordinates persisted")
                 firebase.database().ref("users/" + uid + "/dashboardModel").set({
                     character: model.character,
-                    tasks: model.tasks ,
-                    checkedTasks: model.checkedTasks,
+                    todoList: model.todoList,
                     coordinates: model.coordinates,
                     planet: model.planet,
                     preferences:model.preferences
@@ -21,25 +20,27 @@ export function persistModel(model) {
             }
         });
 
-        firebase.database().ref("users/" + uid + "/dashboardModel").on("value", function (data) {
-            loadingFromFirebase = true;
-            try {
-                if (data.val() && newSession) {
-                    console.log(data.val());
-                    model.setCharacter(data.val().character, data.val().character.id);
-                    model.setTasks(data.val().tasks || [], data.val().checkedTasks  || [])
-                    model.setPlanet(data.val().planet, data.val().planet.id)
-                    model.setAllCoordinates(data.val().coordinates)
-                    model.setPreferences(data.val().preferences)
-                    // We only want to load from firebase once.
-                    newSession = false;
+            firebase.database().ref("users/" + uid + "/dashboardModel").on("value", function (data) {
+                loadingFromFirebase = true;
+                try {
+                    if (data.val() && newSession) {
+                        console.log(data.val());
+                        model.setCharacter(data.val().character, data.val().character.id);
+                        model.setTasks(data.val().todoList.tasks || [], data.val().todoList.checkedTasks || [])
+                        model.setTodoTitle(data.val().todoList.todoTitle || "todo")
+                        model.setPlanet(data.val().planet, data.val().planet.id)
+                        model.setAllCoordinates(data.val().coordinates)
+                        model.setPreferences(data.val().preferences)
+                        // We only want to load from firebase once.
+                        newSession = false;
+                    }
+                } catch (e) {
+                    console.log(e);
+                } finally {
+                    loadingFromFirebase = false;
                 }
-            } catch (e) {
-                console.log(e);
-            } finally {
-                loadingFromFirebase = false;
-            }
 
-        });
+            });
+
     }
 }
