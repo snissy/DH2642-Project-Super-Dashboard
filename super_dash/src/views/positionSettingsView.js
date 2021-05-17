2ec6090f8b9164717720bbe0802acec990bdd48c
snissy <60660773+snissy@users.noreply.github.com>
2021-05-17 10:25:15 +0200

    #none-This adds some feedback to the user that the coordinates are being fetched

--- super_dash/src/views/positionSettingsView.js
+++ super_dash/src/views/positionSettingsView.js
@@ -6,7 +6,7 @@ function PositionSettingsView(props){
         <p> {props.position.geoName}<br/>
             Longitude: {props.position.longitude ? props.position.longitude.toFixed(2): "-"}° <br/>
             Latitude: {props.position.latitude ? props.position.latitude.toFixed(2): "-"}° </p>
-        <Button id={"coordinate-button"} onClick={props.fetchLocation}>Fetch Coordinates</Button>
+        <Button id={"coordinate-button"} onClick={props.fetchLocation} disabled={props.fetchingCoords}> {props.fetchingCoords?"Fetching your position...": "Fetch Position"}</Button>
     </div>)
 }
 
